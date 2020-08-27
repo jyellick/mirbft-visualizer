2a3af0b2ab72b919aaed4c61fcee9c74ad644df9
Jason Yellick <jyellick@us.ibm.com>
2020-08-27 13:04:35 -0400

    Cleanup broken JS from vugu move, add title
    This commit also moves from the old vugu naming convention of foo.vugu
    and foo-data.go to the newer foo.go.
    
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>

--- devserver.go
+++ devserver.go
@@ -15,9 +15,28 @@ func main() {
 
 	wc := devutil.NewWasmCompiler().SetDir(".")
 	mux := devutil.NewMux()
-	mux.Match(devutil.NoFileExt, devutil.DefaultAutoReloadIndex.Replace(
-		`<!-- styles -->`,
-		`<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">`))
+	mux.Match(
+		devutil.NoFileExt,
+		devutil.DefaultAutoReloadIndex.Replace(
+			`<title>Vugu App</title>`,
+			`<title>MirBFT Visualizer</title>`,
+		),
+	)
+	mux.Match(
+		devutil.NoFileExt,
+		devutil.DefaultAutoReloadIndex.Replace(
+			`<!-- styles -->`,
+			`<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">`),
+	)
+	mux.Match(
+		devutil.NoFileExt,
+		devutil.DefaultAutoReloadIndex.Replace(
+			`<!-- scripts -->`,
+			`<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>`,
+		),
+	)
 	mux.Exact("/main.wasm", devutil.NewMainWasmHandler(wc))
 	mux.Exact("/wasm_exec.js", devutil.NewWasmExecJSHandler(wc))
 	mux.Default(devutil.NewFileServer().SetDir("."))
