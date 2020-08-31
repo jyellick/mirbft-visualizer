2a9282b8957244e2b42a278eae197ef5cb9ed6e7
Jason Yellick <jyellick@us.ibm.com>
2020-08-31 15:39:30 -0400

    Fix broken JS and CSS loading
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>

--- devserver.go
+++ devserver.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	l := "127.0.0.1:8844"
-	log.Printf("Starting HTTP Server at %q", l)
+	log.Printf("Starting HTTP Server with mux reload stuff at %q", l)
 
 	wc := devutil.NewWasmCompiler().SetDir(".")
 	mux := devutil.NewMux()
@@ -20,17 +20,9 @@ func main() {
 		devutil.DefaultAutoReloadIndex.Replace(
 			`<title>Vugu App</title>`,
 			`<title>MirBFT Visualizer</title>`,
-		),
-	)
-	mux.Match(
-		devutil.NoFileExt,
-		devutil.DefaultAutoReloadIndex.Replace(
+		).Replace(
 			`<!-- styles -->`,
-			`<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">`),
-	)
-	mux.Match(
-		devutil.NoFileExt,
-		devutil.DefaultAutoReloadIndex.Replace(
+			`<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">`).Replace(
 			`<!-- scripts -->`,
 			`<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
